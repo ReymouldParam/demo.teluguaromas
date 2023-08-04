d6ac3bb98de6d8e617e2a71890b13822aca5162e
ReymouldRevanth <revanth.danduboina@reymould.com>
2023-08-04 17:59:04 +0530

    Loading page added

---
+++ assets/js/global.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", ()=>{
+    document.getElementById("loadingBar").style.width= "100%";
+
+    setTimeout(function(){
+        document.getElementById("preloader").style.display="none";
+    },4000);
+
+    
+});
