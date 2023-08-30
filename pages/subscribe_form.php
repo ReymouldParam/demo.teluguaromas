7a07f80cb4067f77166f9109cb11fa0531f71382
ReymouldRevanth <revanth.danduboina@reymould.com>
2023-08-30 15:39:33 +0530

    Robots.txt file added

--- pages/subscribe_form.php
+++ pages/subscribe_form.php
@@ -1,11 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $to = "contact@reymould.com" ;
+    $to = "contact@teluguaromas.com" ;
     $subject = "Subscription For Telugu aromas From Website";
     $message =  $_POST["email"] . " wants to subscribe to Telugu aromas";
 
     mail($to, $subject, $message);
-    mail("revanth.danduboina@reymould.com",$subject,$message);
+    mail("contact@reymould.com",$subject,$message);
 
     if(mail("revanth.danduboina@reymould.com",$subject,$message)){
         header("Location: home.php?success=trueSubscribe");
