d2416463a17603310d7ee189fc384d3109838bc4
ReymouldRevanth <revanth.danduboina@reymould.com>
2023-09-09 10:14:22 +0530

    Favicon updated for Search engine

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Best Telugu Biryani & Pulav Restaurant in Kompally, Hyderabad</title>
+
+  <!--------------------------- -->
+  <!------------ SEO Meta tags  -->
+  <!--------------------------- -->
+  <meta name="title" content="Best Telugu Biryani & Pulav Restaurant in Kompally, Hyderabad" />
+    <meta name="description" content="Delicious Veg & Non-Veg Bagara Biriyanis and Pulavs at Telugu Aromas | Dining & Take Away | Lunch, Snack & Dinner | Catering Service & Franchise Available" />
+    <meta name="keywords" content="Resturant, Biriyani, Pulavs, Biriyani, Pulav" />
+
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+
+</head>
+<body>
+
 <?php
 
 header("Location: ./pages/home.php");
 exit;
+?>
+    
+</body>
+</html>
+
+
